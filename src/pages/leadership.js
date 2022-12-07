447731714261cb54b1c0de43b5c4e6799131ba45
sowmiyaramraj <sowmiyaramraj.0505@gmail.com>
2022-12-07 10:26:39 +0530

    -

--- src/pages/leadership.js
+++ src/pages/leadership.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -7,7 +7,17 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import Rank from "../images/rank.jpg"
+import axios from "axios";
 function Leadership(){
+  const [leadership,setLeadership]=useState([]);
+  useEffect(()=>{
+    async function getData()
+    {
+        const response= await axios.get("http://localhost:3001/user/get");
+        setLeadership(response.data);    
+    }
+    getData();
+},[]);
     return(
         <div style={{paddingLeft:"50px",paddingRight:"50px"}}>
             <div>
@@ -16,27 +26,25 @@ function Leadership(){
         <div style={{marginLeft:"50px",marginRight:"50px"}}>
             <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} aria-label="caption table">
-        <caption>A basic table example with a caption</caption>
+      
         <TableHead style={{background:"purple"}}>
           <TableRow>
-            <TableCell style={{color:"white"}}>Rank</TableCell>
+            <TableCell style={{color:"white",width:"20px"}}>ID</TableCell>
             <TableCell style={{color:"white"}} align="right">Name</TableCell>
             <TableCell style={{color:"white"}}  align="right">Batch</TableCell>
             <TableCell style={{color:"white"}}  align="right">Learning</TableCell>
             </TableRow>
         </TableHead>
         <TableBody>
-          {/* {rows.map((row) => (
-            <TableRow key={row.name}>
+          {leadership.map((row) => (
+            <TableRow key={row.id}>
               <TableCell component="th" scope="row">
                 {row.name}
               </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
+              <TableCell align="right">{row.batch}</TableCell>
+              <TableCell align="right">{row.learning}</TableCell>
             </TableRow>
-          ))} */}
+          ))}
         </TableBody>
       </Table>
     </TableContainer>
